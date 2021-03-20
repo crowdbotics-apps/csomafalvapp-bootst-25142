d23f0098ab2ef49f6d2eb1476c5cccbcc17c4550
Crowdbotics <team@crowdbotics.com>
2021-03-20 10:38:10 +0000

    add package blueprint-react-native-dashboard-1-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Dashboard11216494', human_name: 'Dashboard 1 1', access_route: 'Dashboard11216494'},
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ]
